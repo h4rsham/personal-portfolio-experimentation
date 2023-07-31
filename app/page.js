a4e60db6c26d105cc1528d99f04fd48aa28fd1f9
h4rsham <harsham.latif@outlook.com>
2023-07-31 16:17:46 +0100

    Changed LinkedIn to Resume

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href="https://www.linkedin.com/in/h4rsham/"
                 >
-                  LinkedIn
+                  Resume
                 </a>
               </li>
             </ul>
